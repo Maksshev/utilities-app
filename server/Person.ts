f15e446f35fc5c3b276b1b7609f78bfd4d20a9ac
Maksym Shevchenko <m.shevchenko@astoundcommerce.com>
2020-06-02 19:03:42 +0300

    Basic TS config

---
+++ server/Person.ts
@@ -0,0 +1,16 @@
+
+export class Person {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+}
---
+++ server/server.ts
@@ -0,0 +1,5 @@
+import {Person} from './Person'
+
+let person: Person = new Person('Valera');
+
+console.log(person.getName());
